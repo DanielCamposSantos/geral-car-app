82136017011ae661d900dc1ab6f3f0f664db8ee8
Daniel <danielcampos.santos1603@gmail.com>
2026-05-08 00:39:13 -0300

    refactor: remoção de imports inutilizados

--- src/app/components/vehicle-card/vehicle-card.ts
+++ src/app/components/vehicle-card/vehicle-card.ts
@@ -1,14 +1,12 @@
-import { Component, computed, input, Input, output } from '@angular/core';
+import { Component, computed, input, Input } from '@angular/core';
 import { VeiculoGetResponse } from '../../models/veiculo-get-response';
 import { environment } from '../../../environments/environment.development';
 import { CommonModule } from '@angular/common';
-import { RouterLink } from "@angular/router";
-import { MenssagemVeiculo } from '../../models/menssagem-veiculo';
 
 
 @Component({
   selector: 'app-vehicle-card',
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule,],
   templateUrl: './vehicle-card.html',
   styleUrl: './vehicle-card.scss',
 })
@@ -17,7 +15,7 @@ export class VehicleCard {
   @Input() veiculo: VeiculoGetResponse | undefined;
 
   realImagePath = computed(() => `${environment.API_URL}/${this.veiculo?.imagens[0].imagePath}`)
-  
+
   whatsappUrl = input.required<string>()
 
 
