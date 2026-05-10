6129cb38b6bce7b7da12b2c5837caa5873dc345e
Daniel <danielcampos.santos1603@gmail.com>
2026-05-10 00:49:34 -0300

    feat: update routing and navigation links in header and footer components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,11 @@
 import { Routes } from '@angular/router';
 import { LandingPage } from './pages/landing-page/landing-page';
 import { Vehicle } from './pages/vehicle/vehicle';
+import { Catalog } from './pages/catalog/catalog';
 
 export const routes: Routes = [
-    {path:"",component:LandingPage},
-    {path:"detalhes/:id",component:Vehicle}
+    { path: "", redirectTo: "home", pathMatch: "full" },
+    { path: "home", component: LandingPage },
+    { path: "catalog", component: Catalog },
+    { path: "detalhes/:id", component: Vehicle }
 ];
